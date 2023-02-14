80e269e060596218ad149560c9476f5ba1d66068
JPLACLAU <laclaujeanpaul@gmail.com>
2023-02-14 01:15:30 -0300

    v0.1.0

---
+++ contracts/Prize4Trash.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+// 1. Pragma
+pragma solidity ^0.8.17;
+
+// 2. Imports
+
+// 3. Interfaces, Libraries, Contracts
+
+/* @title: "A superb Smart Contract"
+ * @author: "My Name here"
+ * @notice: "Super brief explanation of the contract"
+ * @dev: "Super brief technical explanation"
+ */
+
+contract NameOfTheContract {
+    // Type Declarations
+    // State variables
+    // Events
+    // Modifiers
+    // Functions Order:
+    //// constructor
+    //// receive
+    //// fallback
+    //// external
+    //// public
+    //// internal
+    //// private
+    //// view / pure
+}
+
+/*  @param: Explain some param here.
+ *  @notice: Write some short ending comentary here. Be nice.
+ */
